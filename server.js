d21b84204aad80c0f0ed117e673906b16a63b03d
joe mcgee <jrmcgee@asu.edu>
2021-11-01 19:51:34 +0900

    new commit

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get('/api/basketballteams/:teamNames',(request,response)=> {
    
 })
 
-app.listen(PORT, () =>{
+app.listen(process.env.PORT || PORT, () =>{
 console.log(`Server running on port ${PORT}`)
 
 })
